1095938918fe099e10a03ac0790b5764fd5588d6
mursulis <mursulis@oregonstate.edu>
2022-05-19 00:36:09 -0700

    users api and models rough draft

--- api/users.js
+++ api/users.js
@@ -1,7 +1,76 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const {validateAgainstSchema} = require('../lib/validation');
+const { validateAgainstSchema } = require("../lib/validation");
+const { generateAuthToken, requireAuthentication } = require("../lib/auth")
+const { ObjectId } = require("mongodb");
 
-const { ObjectId } = require('mongodb');
+const {
+  userSchema,
+  insertNewUser,
+  getUserById,
+  getUserByEmail,
+  validateUser,
+} = require("../models/users");
 
 exports.router = router;
+
+//In progress
+router.post("/", async function (req, res) {
+  if (validateAgainstSchema(req.body, userSchema)) {
+    try {
+      const id = await insertNewUser(req.body);
+      res.status(201).send({
+        _id: id,
+      });
+    } catch (err) {
+      console.error("  -- Error:", err);
+      res.status(500).send({ error: "Error, user insert unsuccesful." });
+    }
+  } else {
+    res.status(400).send({
+      error: "Request body does not contain a valid User.",
+    });
+  }
+});
+
+//In progress
+router.post("/login", async function (req, res) {
+  if (req.body && req.body.email && req.body.password) {
+    const authenticated = await validateUser(req.body.email, req.body.password);
+    if (authenticated) {
+      const user = await getUserByEmail(req.body.email);
+      console.log("== user.id", user._id);
+      const token = generateAuthToken(user._id);
+      res.status(200).send({ token: token });
+    } else {
+      res.status(401).send({
+        error: "Invalid credentials",
+      });
+    }
+  } else {
+    res.status(400).send({
+      error: "Request needs email and password.",
+    });
+  }
+});
+
+//In progress
+router.get("/:id", requireAuthentication, async function (req, res, next) {
+  console.log("== req.user:", req.user);
+  console.log("== req.params.id:", req.params.id);
+
+  if (req.user !== req.params.id) {
+    res.status(403).send({
+      err: "Unauthorized to access the specified resource",
+    });
+    next();
+  } else {
+    const user = await getUserById(req.params.id);
+    console.log("== req.headers:", req.headers);
+    if (user) {
+      res.status(200).send(user);
+    } else {
+      next();
+    }
+  }
+});
---
+++ models/users.js
@@ -0,0 +1,71 @@
+const { ObjectId } = require("mongodb");
+const { getDbInstance } = require("../lib/mongo");
+const { extractValidFields } = require("../lib/validation");
+
+const bcrypt = require("bcryptjs");
+
+const userSchema = {
+  name: { required: true },
+  email: { required: true },
+  password: { required: true },
+  role: { required: true },
+};
+exports.userSchema = userSchema;
+
+//TEST
+exports.getUsers = async function getUsers() {
+  const db = getDbInstance();
+  const collection = db.collection("users");
+
+  const results = await collection.find({}).toArray();
+  return results;
+};
+
+//
+exports.insertNewUser = async function insertNewUser(user) {
+  const userToInsert = extractValidFields(user, userSchema);
+  const db = getDbInstance();
+  const collection = db.collection("users");
+
+  //password hash
+  const passwordHash = await bcrypt.hash(userToInsert.password, 8);
+  userToInsert.password = passwordHash;
+  console.log("== Hashed, salted password:", userToInsert.password);
+
+  const result = await collection.insertOne(userToInsert);
+  return result.insertedId;
+};
+
+//
+exports.getUserById = async function getUserById(id, includePassword) {
+  const db = getDbInstance();
+  const collection = db.collection("users");
+  if (!ObjectId.isValid(id)) {
+    return null;
+  } else {
+    const results = await collection
+      .find({ _id: new ObjectId(id) })
+      .project(includePassword ? {} : { password: 0 })
+      .toArray();
+    return results[0];
+  }
+};
+
+//
+exports.validateUser = async function validateUser(email, password) {
+  const user = await getUserByEmail(email, true);
+  const authenticated = user && (await bcrypt.compare(password, user.password));
+  return authenticated;
+};
+
+//
+async function getUserByEmail(email, includePassword) {
+  const db = getDbInstance();
+  const collection = db.collection("users");
+  const results = await collection
+    .find({ email: email })
+    .project(includePassword ? {} : { password: 0 })
+    .toArray();
+  return results[0];
+};
+exports.getUserByEmail = getUserByEmail;
