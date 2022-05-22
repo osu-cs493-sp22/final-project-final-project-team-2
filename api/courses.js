9416a800c48a80282c999bb6ede1682d8a9c5f62
PeterJBloch <peterjbloch@gmail.com>
2022-05-22 14:19:20 -0700

    new course creation should be possible

--- api/courses.js
+++ api/courses.js
@@ -1,7 +1,24 @@
 const router = require('express').Router();
 
-const {validateAgainstSchema} = require('../lib/validation');
+const {validateAgainstSchema, extractValidFields} = require('../lib/validation');
 
 const { ObjectId } = require('mongodb');
+const { requireAuthentication } = require('../lib/auth');
+const { insertNewCourse } = require('../models/courses');
+const { CourseSchema } = requre("../models/courses");
 
 exports.router = router;
+
+router.post("/", requireAuthentication, async function (req, res) {
+    //only admins can use this function
+    if (req.user.role != "admin"){
+        res.status(403).send({
+            err: "You do not have the permissions to add a new course"
+        })
+        return
+    }
+    if (validateAgainstSchema(req.body, CourseSchema)) {
+        const course = extractValidFields(req.body,CourseSchema)
+        insertNewCourse(course);
+    }
+});
